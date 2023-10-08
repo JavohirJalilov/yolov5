159bd7e2d2120ff424f60471ddb788bdde464232
JavohirJalilov <javohirjaliloff@gmail.com>
2023-10-08 13:13:43 +0500

    fix

--- models/common.py
+++ models/common.py
@@ -330,7 +330,7 @@ class ECA(nn.Module):
         k_size: Adaptive selection of kernel size
     """
 
-    def __init__(self, c1,c2, k_size=3):
+    def __init__(self, c1, k_size=3):
         super(ECA, self).__init__()
         self.avg_pool = nn.AdaptiveAvgPool2d(1)
         self.conv = nn.Conv1d(1, 1, kernel_size=k_size, padding=(k_size - 1) // 2, bias=False)
